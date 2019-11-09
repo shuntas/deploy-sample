958db3d284184116f6f77b1e68abd046075fd286
shuntas <shunta.suzuki@di-v.co.jp>
2019-11-09 22:19:58 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.11.2'
 set :application, 'deploy-sample'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:shunats/deploy-sample.git'
+set :repo_url,  'git@github.com:shuntas/deploy-sample.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
